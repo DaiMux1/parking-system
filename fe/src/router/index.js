2f135a1905407cb2249f44957d2bdc9da151ae4c
dai02012000 <ledai02012000@gmail.com>
2021-12-15 15:45:39 +0700

    fix login

--- fe/src/router/index.js
+++ fe/src/router/index.js
@@ -5,14 +5,32 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const routes = [
+
     {
         path: "/",
         name: "Home",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
+
         component: () =>
             import(/* webpackChunkName: "about" */ "../views/Home.vue"),
+    },
+    {
+        path: "/404",
+        name: "404Page",
+
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/404Page.vue"),
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/Login.vue"),
+    },
+    {
+        path: "/register",
+        name: "Register",
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/Register.vue"),
     },
     {
         path: "/manage-account",
