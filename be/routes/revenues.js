56e2b91c923c54ba531ca96ce3889d4a0e7402dd
dai02012000 <ledai02012000@gmail.com>
2021-12-26 16:35:27 +0700

    update revenues

--- be/routes/revenues.js
+++ be/routes/revenues.js
@@ -7,11 +7,38 @@ const router = express.Router();
 // tinh doanh thu nhập vào ngày tháng
 router.get('/', async (req, res) => {
   if (!req.query.day && !req.query.month) {
-    return res.status(400).send('No day, month')
+    // return res.status(400).send('No day, month')
+    const result = []
+    for (let month of [...Array(6).keys()]) {
+      let firstDate = new Date(new Date().getFullYear(), new Date().getMonth(), 1, 7)
+      firstDate.setMonth(firstDate.getMonth() - month)
+      let endDate = new Date(new Date().getFullYear(), new Date().getMonth(), 30, 7)
+      endDate.setMonth(firstDate.getMonth() - month)
+      let revenues = await Revenue.find({
+        time: {
+          $gte: firstDate,
+          $lt: endDate,
+        }
+      });
+
+      let revenue = 0
+      let day_ticket = 0
+      let month_ticket = 0
+      for (const i in revenues) {
+        revenue = revenue + revenues[i].revenue
+        if (revenues[i].ticket_type == "ngay") day_ticket += 1
+        else month_ticket += 1
+      }
+      result.push({ revenue: revenue, month: firstDate.getMonth() + 1, year: firstDate.getFullYear(), day_ticket: day_ticket, month_ticket: month_ticket })
+    }
+
+    res.send(result)
+
   }
 
   month = req.query.month ? req.query.month - 1 : (new Date().getMonth())
   month = parseInt(month)
+  let year = req.query.year ? req.query.year : new Date().getFullYear()
 
 
   let revenues = []
@@ -43,45 +70,42 @@ router.get('/', async (req, res) => {
     });
   }
   let revenue = 0
+  let day_ticket = 0
+  let month_ticket = 0
   for (const i in revenues) {
     revenue = revenue + revenues[i].revenue
+    if (revenues[i].ticket_type == "ngay") day_ticket += 1
+    else month_ticket += 1
   }
 
-  res.json({ revenue: revenue });
+  res.json({ revenue: revenue, month: month + 1, year: year, day_ticket: day_ticket, month_ticket: month_ticket });
 });
 
 // thống kê số lượng vé tháng vé ngày
 router.get('/statistic', async (req, res) => {
-  if (!req.query.day && !req.query.month) {
-    return res.status(400).send('No day, month')
-  }
+  // if (!req.query.day && !req.query.month) {
+  //   return res.status(400).send('No day, month')
+  // }
 
-  month = req.query.month ? req.query.month - 1 : (new Date().getMonth())
+  let month = req.query.month ? req.query.month - 1 : (new Date().getMonth())
   month = parseInt(month)
+  let year = req.query.year ? req.query.year : (new Date().getFullYear())
+  year = parseInt(year)
+  let day = req.query.day ? parseInt(req.query.day) : null
 
-  if (req.query.day && req.query.year) {
-    let day = parseInt(req.query.day)
-    let year = parseInt(req.query.year)
+  if (day) {
     revenues = await Revenue.find({
       time: {
         $gte: new Date(year, month, day, 7),
         $lt: new Date(year, month, day + 1, 7),
       }
     });
-  } else if (req.query.day) {
-    let day = parseInt(req.query.day)
-    revenues = await Revenue.find({
-      time: {
-        $gte: new Date(new Date().getFullYear(), month, day, 7),
-        $lt: new Date(new Date().getFullYear(), month, day + 1, 7),
-      }
-    });
   }
   else {
     revenues = await Revenue.find({
       time: {
-        $gte: new Date(new Date().getFullYear(), month, 1, 7),
-        $lt: new Date(new Date().getFullYear(), month, 30, 7),
+        $gte: new Date(year, month, 1, 7),
+        $lt: new Date(year, month, 30, 7),
       }
     });
   }
@@ -93,7 +117,7 @@ router.get('/statistic', async (req, res) => {
     else month_ticket += 1
   }
 
-  res.json({ ve_ngay: day_ticket, ve_thang: month_ticket })
+  res.json({ day_ticket: day_ticket, month_ticket: month_ticket, day: day, month: month+1, year: year })
 })
 
 
@@ -110,6 +134,7 @@ router.post('/', async (req, res) => {
   await revenue.save()
   res.send(revenue)
 })
+
 
 
 module.exports = router
