cd96792243089f1c37de7456c39268920c48231a
thanhdt300999 <thanh.dt300999@gmail.com>
2022-01-18 00:05:06 +0700

    fix login screen, fix style

--- fe/src/views/Login.vue
+++ fe/src/views/Login.vue
@@ -3,17 +3,16 @@
     <div class="container">
       <!-- Outer Row -->
       <div class="row justify-content-center">
-        <div class="col-xl-10 col-lg-12 col-md-9">
+        <div class="col-xl-7 col-lg-12 col-md-9">
           <div class="card o-hidden border-0 shadow-lg my-5">
-            <div class="card-body p-0">
+            <div class="card-body p-1">
               <!-- Nested Row within Card Body -->
               <div class="row">
-                <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                <div class="col-lg-6">
+                <div class="col-lg-12">
                   <div class="p-5">
                     <div class="text-center">
                       <h1 class="h4 text-gray-900 mb-4">
-                        Chào mừng đã trở lại !!!!
+                        Chào mừng đã trở lại hệ thống gửi xe !!!
                       </h1>
                     </div>
                     <form class="user">
@@ -26,6 +25,7 @@
                           aria-describedby="emailHelp"
                           placeholder="Tài khoản"
                         />
+                        <p class="text-center" v-if="username.length < 5" style="color:red; margin-top: 2%;">Tài khoản cần ít nhất 5 ký tự</p>
                       </div>
                       <div class="form-group">
                         <input
@@ -35,6 +35,7 @@
                           id="exampleInputPassword"
                           placeholder="Mật khẩu"
                         />
+                        <p class="text-center" v-if="password.length < 5" style="color:red; margin-top: 2%;">Mật khẩu cần ít nhất 5 ký tự</p>
                       </div>
 
                        <a v-on:click="handleSubmit" class="btn btn-primary btn-user btn-block">
@@ -44,11 +45,6 @@
                     <hr />
                     <div class="text-center">
                       <a class="small" href="#">Quên mật khẩu?</a>
-                    </div>
-                    <div class="text-center">
-                      <router-link :to="{ name: 'Register' }" class="small"
-                        >Tạo tài khoản Admin!</router-link
-                      >
                     </div>
                   </div>
                 </div>
@@ -98,7 +94,7 @@ export default {
           }
         }
       } catch (err) {
-        alert(err);
+        alert(err.response.data);
       }
     },
     ...mapActions('account', ["submitUser"]),
@@ -106,4 +102,11 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.bg-gradient-primary {
+  display: flex;
+  height: 100%;
+  align-items: center;
+  background-image: url("../assets/imgs/img-background.jpg");
+}
+</style>
